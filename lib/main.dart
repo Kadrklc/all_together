28280e4c7d3bbbb1f9a48d95dbaaf33dd2b01719
AKK <kadir2257@gmail.com>
2023-04-03 00:11:09 +0300

    add fribase

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,12 @@ import 'package:all_together/components/my_button.dart';
 import 'package:all_together/components/my_textfield.dart';
 import 'package:all_together/components/square_tile.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(),,,,,,,,,,,,,,,,
   runApp(LoginPage());
 }
 
