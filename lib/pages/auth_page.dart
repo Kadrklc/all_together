a5c90f309f455f50283f4e8eb903c8d7867fb55c
AKK <kadir2257@gmail.com>
2023-04-06 23:21:56 +0300

    reflesh

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -2,15 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthPage extends StatelessWidget {
-  const AuthPage
-({super.key});
+  const AuthPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:StreamBuilder<User?>(
+      body: StreamBuilder<User?>(
         stream: FirebaseAuth,
-        ),
+      ),
     );
   }
 }
